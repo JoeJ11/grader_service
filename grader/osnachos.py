1027348bd8b5cbb346d701877062df7875a44a71
JoeJ11 <andyjvan@hotmail.com>
2016-03-24 21:28:42 +0800

    Update nachos grading script.

--- grader/osnachos.py
+++ grader/osnachos.py
@@ -1,21 +1,97 @@
 import os
 import sys
 import json
+import subprocess
+
+import process
+RAILS_ROOT = '/home/ubuntu/grader_service/'
+GRADER_ROOT = os.path.join(RAILS_ROOT, 'nachos')
 
 tmp_dir_path = sys.argv[1]
 with open(os.path.join(tmp_dir_path, 'answer'), 'r') as f_in:
 	user_info = json.load(f_in)
 
 proj_name = '{}_osnachos_code'.format(user_info['group_name'])
-os.chdir(os.path.join('/home/ubuntu/grader_service/', 'osnachos'))
-if not os.isdir(proj_name):
-	os.system('git clone http://root:passw0rd@10.2.1.88/{} {}_osnachos_code'.format(user_info['git_location'], user_info['group_name']))
+os.chdir(os.path.join(RAILS_ROOT, 'osnachos'))
+if not os.path.isdir(proj_name):
+	os.system('git clone http://root:passw0rd@10.2.1.88/{} {}'.format(user_info['git_location'], proj_name))
+	os.chdir(proj_name)
 else:
 	os.chdir(proj_name)
 	os.system('git pull')
 
+os.system('cp -r threads/ {}/nachos/'.format(GRADER_ROOT))
+os.chdir(os.path.join(GRADER_ROOT, 'proj1'))
+
+os.system('make clean')
+os.system('make')
+os.system('make ag')
+
+score = 0
+comment = ''
+tasks = {
+	1:'../bin/nachos -- nachos.ag.ThreadGrader3 -# joinAfterFinish=True',
+	2:'../bin/nachos -- nachos.ag.ThreadGrader1 -# level=20',
+	3:'../bin/nachos -- nachos.ag.ThreadGrader4 -# numThreads=3,requireImmediate=false',
+	4:'../bin/nachos -- nachos.ag.ThreadGrader2 -# goal=10,numThreads=10',
+	5:'../bin/nachos -- nachos.ag.ThreadGrader5 -# checkPriority=true,checkFifo=true,alterPriority=true',
+	6:'../bin/nachos -- nachos.ag.ThreadGrader6a -# lottery=false,depth=5,fanout=3,maxLocks=2,pJoin=50,release=5',
+	7:'../bin/nachos -- nachos.ag.ThreadGrader6b -# depth=10,pJoin=50',
+	8:'../bin/nachos  — nachos.ag.BoatGrader2 -# children=2,adults=1,priorities=3',
+}
+
+def grade(task):
+	terminate, ext_code, out, err = process.exec_command(tasks[task])
+	print 'Task {}'.format(task)
+	if 'success' in out.split('\n'):
+		return True, 'Test Case {}: Success!\n'.format(task)
+	elif terminate:
+		return False, 'Test Case {} Error Message:\n{}'.format(task, err)
+	else:
+		return False, 'Test Case {} Timeout\n'.format(task)
+
+process.exec_command('cp nachos.conf1 nachos.conf')
+correct, msg = grade(1)
+if correct:
+	score += 1
+comment += msg
+correct, msg = grade(2)
+if correct:
+	score += 1
+comment += msg
+correct, msg = grade(3)
+if correct:
+	score += 1
+comment += msg
+correct, msg = grade(4)
+if correct:
+	score += 1
+comment += msg
+process.exec_command('cp nachos.conf2 nachos.conf')
+correct, msg = grade(5)
+if correct:
+	score += 1
+comment += msg
+correct, msg = grade(6)
+if correct:
+	score += 1
+comment += msg
+correct, msg = grade(7)
+if correct:
+	score += 1
+comment += msg
+process.exec_command('cp nachos.conf1 nachos.conf')
+terminate, ext_code, out, err = process.exec_command(tasks[8])
+if terminate:
+	score += 1
+	comment += 'Test Case 8: Success!\n'
+else:
+	comment += 'Test Case 8: Timeout\n'
+
 os.chdir(tmp_dir_path)
+print 'Grade {}/{}'.format(score, len(tasks))
+print 'Comment:\n{}'.format(comment)
 with open('response', 'w') as f_out:
-	f_out.write('10\n')
-	f_out.write('10\n')
-	f_out.write('Good job!\n')
+	f_out.write('{}\n'.format(score))
+	f_out.write('{}\n'.format(len(tasks)))
+	f_out.write(comment)
---
+++ grader/process.py
@@ -0,0 +1,15 @@
+import subprocess
+import time
+
+def exec_command(command):
+	proc = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+	counter = 0
+	while proc.poll()==None and counter < 30:
+		counter += 1
+		time.sleep(1)
+	if proc.poll()==None:
+		proc.terminate()
+		return (False, proc.poll(), '', '')
+	else:
+		(out, err) = proc.communicate()
+		return (True, proc.poll(), out, err)
