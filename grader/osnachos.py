a1421293eaee9291aae377b54d075aab81ba0385
JoeJ11 <andyjvan@hotmail.com>
2016-03-24 13:33:17 +0800

    Fix path.

--- grader/osnachos.py
+++ grader/osnachos.py
@@ -7,7 +7,7 @@ with open(os.path.join(tmp_dir_path, 'answer'), 'r') as f_in:
 	user_info = json.load(f_in)
 
 proj_name = '{}_osnachos_code'.format(user_info['group_name'])
-os.chdir(os.path.join(tmp_dir_path, '..', 'osnachos'))
+os.chdir(os.path.join('/home/ubuntu/grader_service/', 'osnachos'))
 if not os.isdir(proj_name):
 	os.system('git clone http://root:passw0rd@10.2.1.88/{} {}_osnachos_code'.format(user_info['git_location'], user_info['group_name']))
 else:
